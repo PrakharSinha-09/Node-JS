949ce3e49954d8c8008184fd42487a821a724b8f
Prakhar Sinha <prakhar.sinha2k2@gmail.com>
2023-03-10 10:22:22 +0530

    Rendering HTML without passing extension in URL + 404 Error

--- Express_JS/Rendering_HTML/index.js
+++ Express_JS/Rendering_HTML/index.js
@@ -8,6 +8,20 @@ const publicPath=path.join(__dirname,'Public')
 
 // console.log(publicPath)
 
-app.use(express.static(publicPath))               //this 'use' is a middleware, will see in details later.....static helps to load static pages/content
+// app.use(express.static(publicPath))               //this 'use' is a middleware, will see in details later.....static helps to load static pages/content
+
+//TO Render Without Extension, we will not use -->  app.use(express.static(publicPath))
+app.get('/home',(req,res)=>{
+    res.sendFile(`${publicPath}/home.html`)
+})
+app.get('/about',(req,res)=>{
+    res.sendFile(`${publicPath}/about.html`)
+})
+app.get('',(req,res)=>{
+    res.sendFile(`${publicPath}/index.html`)
+})
+app.get('*',(req,res)=>{
+    res.sendFile(`${publicPath}/noPage.html`)
+})
 
 app.listen(3000)
