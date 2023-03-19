efd1e598779f82b75b2b5d3dc3cab5a7e8e15554
Prakhar Sinha <prakhar.sinha2k2@gmail.com>
2023-03-19 17:34:56 +0530

    Starting With Mongoose!

---
+++ Mongoose/index.js
@@ -0,0 +1,25 @@
+//Mongoose is a npm package through which we can connect our mongodb database with nodejs just like we have previously used mongodb npm package
+//the difference is that, mongoose provides additional functionalities like schema creation, validation etc which traditional mongodb package
+//doesn't provide.
+
+//the Step - 1 is to get the mongoose installed and import it here like this
+const mongoose=require('mongoose')
+
+const main=async ()=>{
+    //Step - 2
+    await mongoose.connect("mongodb://localhost:27017/e-comm")               //e-comm is the name of database
+    const productSchema=new mongoose.Schema({                                //creating schema
+        name:String                                                          //one field named 'name' added in schema
+     })
+
+    const ProductsModel=mongoose.model('products',productSchema)             //first argument is the name of the collection, other is the schema
+    
+    //Adding data, whenever adding new data, you have to create an instance, shown below!
+
+    let data=new ProductsModel({name:"OnePlus 11"})                          //Since we have only defined name in schema, if we try to add here some more, it won't get added in database
+    let result=await data.save()                                            //To Save The changes in database
+
+    console.log(result)
+}
+
+main()
